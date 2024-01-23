b31d35d21108b35ba435903fb1ab22927209d21a
Sushil <kidsushilink@gmail.com>
2024-01-22 23:41:48 -0800

    JavaScript
    Refresh

--- day1/src/App.js
+++ day1/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learning React Day 1
         </a>
       </header>
     </div>
