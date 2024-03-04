5baa757cec45513ec53735f04f4416cb5c97a61f
Sushil <kidsushilink@gmail.com>
2024-03-04 00:29:53 -0800

    started mosh tutorial

---
+++ tutorialMosh/src/App.tsx
@@ -0,0 +1,9 @@
+import Message from "./Message";
+import ListGroup from "./components/ListGroup";
+
+function App() {
+  return <Message />
+  return <ListGroup />
+}
+
+export default App;
---
+++ tutorialMosh/src/Message.tsx
@@ -0,0 +1,10 @@
+
+
+function Message(){
+    const name = "";
+    if(name)
+        return <h1>hello {name}</h1>
+    return <h1>Hello world</h1>
+}
+
+export default Message
