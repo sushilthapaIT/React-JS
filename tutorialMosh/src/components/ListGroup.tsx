d1d529b9ee7149d8975a14c64336c3272a7f9e9d
Sushil <kidsushilink@gmail.com>
2024-03-05 23:56:26 -0800

    UPDATE

--- tutorialMosh/src/components/ListGroup.tsx
+++ tutorialMosh/src/components/ListGroup.tsx
@@ -1,7 +1,24 @@
-
-
 function ListGroup() {
-    return <h1>List Group</h1>;
-}
-
-export default ListGroup;
+    let items = ["New York", "Nepal", "Canada", "India", "London"];
+    
+    return (
+    <>
+    <h1>List</h1>
+    <ul className="list-group">
+    {/* {items.length === 0 ? <p>No data found</p> : null} */}
+    {items.length === 0 && <p>No dataa Found</p>}
+    {items.map((item) => (
+    <li
+    className="list-group-item"
+    key={item}
+    onClick={() => console.log("Clicked")}
+    >
+    {item}
+    </li>
+    ))}
+    </ul>
+    </>
+    );
+    }
+    
+    export default ListGroup;
