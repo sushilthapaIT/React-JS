df25b99f220807d54f816fb46ac247f0d38a6a92
Sushil <kidsushilink@gmail.com>
2024-03-24 23:27:47 -0700

    Card Component
    -- learned about card component

---
+++ bro-code/src/Card.jsx
@@ -0,0 +1,13 @@
+import profilePic from './assets/Capture.PNG';
+
+function Card(){
+    return(
+        <div className="card">
+            <img src={profilePic} alt="my image"></img>
+            <h2>Susi</h2>
+            <p>I am studying full stack software developer</p>
+        </div>
+    );
+}
+
+export default Card
---
+++ bro-code/src/Footer.jsx
@@ -0,0 +1,10 @@
+
+function Footer(){
+    return(
+        <footer>
+            <>&copy; {new Date().getFullYear()} Sushil Thapa</>
+        </footer>
+    );
+}
+
+export default Footer
