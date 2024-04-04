9cf9d6e94a6f621f6db214fc5c6c0f7f3fa2d0d2
Sushil <kidsushilink@gmail.com>
2024-04-04 00:59:19 -0700

    update on updatearraysinstate.jsx

--- bro-code/src/UpdateArraysInState.jsx
+++ bro-code/src/UpdateArraysInState.jsx
@@ -4,6 +4,19 @@ function UpdateArrayInState(){
 
     const [foods, setFood] = useState(["apple", "banana", "pineapple", "watermelon"]);
 
+    function handleAddFood(){
+        const newFood = document.getElementById("foodInput").value;
+
+        document.getElementById("foodInput").value = "";
+
+
+
+    }
+
+    function handleRemoveFood(){
+
+    }
+
     return(
         <>
             <h2>List of foods</h2>
@@ -11,8 +24,8 @@ function UpdateArrayInState(){
                 {foods.map((food, index) => [<li key={index}>{food}</li>])}
             </ul>
 
-            <input />
-            <button>Add Food</button>
+            <input type="text" id="foodInput" placeholder="Enter the food name"/>
+            <button onClick={handleAddFood}>Add Food</button>
         </>
     );
 
