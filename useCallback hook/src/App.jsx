ddbf5a5b60f5cf5782d0d6a24f15ac51c0030b75
Sushil <kidsushilink@gmail.com>
2024-08-17 14:42:17 -0700

    added new component to learn about use of useCallback hook with realtime example

--- useCallback hook/src/App.jsx
+++ useCallback hook/src/App.jsx
@@ -2,12 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './Components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [adjective, setAdjective] = useState("good");
+
+  const getAdjective = () => {
+    return "another"
+  }
 
   return (
     <>
+    <Navbar adjective={"good"} getAdjective={getAdjective}/>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
