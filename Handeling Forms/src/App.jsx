7fca8ced77ae219916e9531d89d4e257ad1453c5
Sushil <kidsushilink@gmail.com>
2024-08-24 03:03:41 -0700

    added useForm boiler code

--- Handeling Forms/src/App.jsx
+++ Handeling Forms/src/App.jsx
@@ -1,7 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import {useForm} from "react-hook-form"
 
 function App() {
+
+  const { 
+    register,
+    handleSubmit,
+    watch,
+    formState: {errors},
+  } = useForm()
 
   return (
     <>
